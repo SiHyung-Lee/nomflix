039373bc7db7e164cd9671ad9d772db55a342d04
SiHyeongLee <buzzcatfish@gmail.com>
2019-06-10 09:50:22 +0900

    add api

--- README.md
+++ README.md
@@ -11,8 +11,11 @@ Learning React and ES6 by building a Movie Discovery App.
 
 ## API Verbs
 
-- [ ] Now playing (Movie)
-- [ ] upcoming (Movie)
-- [ ] Top Rated (TV, Movie)
-- [ ] Popular (TV, Movie)
-- [ ] Airing Today (TV)
+- [x] Now playing (Movie)
+- [x] upcoming (Movie)
+- [x] Top Rated (TV, Movie)
+- [x] Popular (TV, Movie)
+- [x] Airing Today (TV)
+- [x] TV Show Detail
+- [x] Movie Detail
+- [x] Search (Movie, TV)
--- src/api.js
+++ src/api.js
@@ -1,13 +1,47 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://api.themoviedb.org/3/",
+    baseURL: "https://api.themoviedb.org/3/movie/popular?api_key=df2d4dee44d982356f572c174844005c&language=en-US&page=1\n",
     params: {
         api_key: "df2d4dee44d982356f572c174844005c",
         language: "en-US"
     }
 });
 
-api.get("tv/popular");
+export const moviesApi = {
+    nowPlaying: () => api.get("movie/now_playing"),
+    upcomimg: () => api.get("movie/upcoming"),
+    popular: () => api.get("movie/popular"),
+    movieDetail: id =>
+        api.get(`movie/${id}`, {
+            params: {
+                append_to_response: "videos"
+            }
+        }),
+    search: term =>
+        api.get("search/movie", {
+            params: {
+                query: encodeURIComponent(term)
+            }
+        })
+};
 
-export default api;
+export const tvApi = {
+    topRated: () => api.get("tv/top_rated"),
+    popular: () => api.get("tv/popular"),
+    airingToday: () => api.get("tv/airing_today"),
+    showDetail: id =>
+        api.get(`tv/${id}`, {
+            params: {
+                append_to_response: "videos"
+            }
+        }),
+    search: term =>
+        api.get("search/tv", {
+            params: {
+                query: encodeURIComponent(term)
+            }
+        })
+};
+
+console.log(tvApi.topRated());
