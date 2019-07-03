1ef363d50a802da6b6b99395cf66cc88deea2e19
isihyeong <buzzcatfish@gmail.com>
2019-07-04 00:37:35 +0900

    home presenter

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Section from 'Components/Section';
 
-const HomePresenter = ({nowPlaying, popular, upcoming, loading, error}) => null;
+const Container = styled.div`
+  padding:0 10px;
+`;
+
+const HomePresenter = ({nowPlaying, popular, upcoming, loading, error}) =>
+    loading ? null : (
+        <Container>
+            {nowPlaying && nowPlaying.length > 0 && (
+                <Section title="Now Playing">
+                    {nowPlaying.map(movie => movie.title)}
+                </Section>
+            )}
+            {upcoming && upcoming.length > 0 && (
+                <Section title="Upcoming Movies">
+                    {upcoming.map(movie => movie.title)}
+                </Section>
+            )}
+            {popular && popular.length > 0 && (
+                <Section title="Popular Movies">
+                    {popular.map(movie => movie.title)}
+                </Section>
+            )}
+        </Container>
+    );
 
 HomePresenter.propTypes = {
-    nowPlaying:PropTypes.array,
-    popular:PropTypes.array,
-    upcoming:PropTypes.array,
-    loading:PropTypes.bool.isRequired,
-    error:PropTypes.string
+    nowPlaying: PropTypes.array,
+    popular: PropTypes.array,
+    upcoming: PropTypes.array,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.string
 };
 
 export default HomePresenter;
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -2,14 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const SearchPresenter = ({
-                             movieResults,
-                             tvResults,
-                             loading,
-                             searchTerm,
-                             handleSubmit,
-                             error
-                         }) => null;
+const SearchPresenter = ({movieResults, tvResults, loading, searchTerm, handleSubmit, error}) => null;
 
 SearchPresenter.propTypes = {
     movieResults: PropTypes.array,
